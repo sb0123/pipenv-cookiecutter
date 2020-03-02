ae3afdd4a3a56892d5e6ab9e48407877f36d289c
André Cloete <ajcloete@gmail.com>
2020-03-02 16:49:22 +0000

    fix: all strings now use double quotes in setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -3,48 +3,49 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
 from typing import List
 
-with open('README.rst') as readme_file:
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=6.0',{%- endif %} ] # type: List[str]
+requirements = [{%- if cookiecutter.command_line_interface|lower == "click" %}"Click>=6.0",{%- endif %} ] # type: List[str]
 
-setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %} ] # type: List[str]
+setup_requirements = [{%- if cookiecutter.use_pytest == "y" %}"pytest-runner",{%- endif %} ] # type: List[str]
 
-test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest',{%- endif %} ] # type: List[str]
+test_requirements = [{%- if cookiecutter.use_pytest == "y" %}"pytest",{%- endif %} ] # type: List[str]
 
 {%- set license_classifiers = {
-    'MIT license': 'License :: OSI Approved :: MIT License',
-    'BSD license': 'License :: OSI Approved :: BSD License',
-    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
-    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
-    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    "MIT license": "License :: OSI Approved :: MIT License",
+    "BSD license": "License :: OSI Approved :: BSD License",
+    "ISC license": "License :: OSI Approved :: ISC License (ISCL)",
+    "Apache Software License 2.0": "License :: OSI Approved :: Apache Software License",
+    "GNU General Public License v3": "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
 } %}
 
 setup(
     author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
-    author_email='{{ cookiecutter.email }}',
+    author_email="{{ cookiecutter.email }}",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
 {%- if cookiecutter.open_source_license in license_classifiers %}
-        '{{ license_classifiers[cookiecutter.open_source_license] }}',
+        "{{ license_classifiers[cookiecutter.open_source_license] }}",
 {%- endif %}
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="{{ cookiecutter.project_short_description }}",
-    {%- if 'no' not in cookiecutter.command_line_interface|lower %}
+    {%- if "no" not in cookiecutter.command_line_interface|lower %}
     entry_points={
-        'console_scripts': [
-            '{{ cookiecutter.repo_name }}={{ cookiecutter.repo_name }}.cli:main',
+        "console_scripts": [
+            "{{ cookiecutter.repo_name }}={{ cookiecutter.repo_name }}.cli:main",
         ],
     },
     {%- endif %}
@@ -54,13 +55,13 @@ setup(
 {%- endif %}
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='{{ cookiecutter.repo_name }}',
-    name='{{ cookiecutter.repo_name }}',
-    packages=find_packages(include=['{{ cookiecutter.repo_name }}']),
+    keywords="{{ cookiecutter.repo_name }}",
+    name="{{ cookiecutter.repo_name }}",
+    packages=find_packages(include=["{{ cookiecutter.repo_name }}"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
-    version='{{ cookiecutter.version }}',
+    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}",
+    version="{{ cookiecutter.version }}",
     zip_safe=False,
 )
