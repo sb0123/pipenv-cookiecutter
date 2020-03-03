0a68bb8a7180101098c2d5d75f37044aea370758
AJ Cloete <ajcloete@gmail.com>
2020-03-03 20:04:12 +0000

    Update cli.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/cli.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/cli.py
@@ -1,16 +1,16 @@
 
 # -*- coding: utf-8 -*-
 
-"""Console script for {{cookiecutter.project_slug}}."""
+"""Console script for {{cookiecutter.repo_name}}."""
 import sys
 import click
 
 
 @click.command()
 def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
+    """Console script for {{cookiecutter.repo_name}}."""
     click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
+               "{{cookiecutter.repo_name}}.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
