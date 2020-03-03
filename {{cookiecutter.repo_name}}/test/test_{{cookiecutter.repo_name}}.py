2debfe9c80b88237fc9a9ef93b6b9cecdb192944
AJ Cloete <ajcloete@gmail.com>
2020-03-03 20:35:41 +0000

    Update test_{{cookiecutter.repo_name}}.py

--- {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
@@ -1,5 +1,6 @@
-from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}} import fib
 from {{cookiecutter.repo_name}} import cli
+from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}} import fib
+
 
 def test_fib() -> None:
     assert fib(0) == 0
@@ -9,6 +10,7 @@ def test_fib() -> None:
     assert fib(4) == 3
     assert fib(5) == 5
     assert fib(10) == 55
+
 
 def test_cli() -> None:
     assert cli.main() == 0
