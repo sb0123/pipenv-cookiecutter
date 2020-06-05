f5a7779c27ba609915cd99bff36c23d66376f9ae
André Cloete <andre.cloete@anmut.co.uk>
2020-06-05 10:38:57 +0100

    add tags push to git-release.sh

--- {{cookiecutter.repo_name}}/scripts/git-release.sh
+++ {{cookiecutter.repo_name}}/scripts/git-release.sh
@@ -4,10 +4,10 @@ echo "\n-> Tagging $1 release\n" \
 && pipenv run make_docs \
 && echo "\n-> docs made! use `open docs/_build/html/index.html` to view"
 && echo "\n-> pushing to remote, including tags\n" \
-&& pipenv run git push origin master \
+&& pipenv run git push origin master --tags \
 && echo "\n######################\nNEW RELEASE SUCCESSFUL\n######################\n" \
 || (git add Pipfile.lock \
     && git commit -m "Update Pipfile.lock" \
-    && pipenv run git push origin master \
+    && pipenv run git push origin master --tags \
     && echo "\n######################\nNEW RELEASE SUCCESSFUL\n######################\n") \
 || echo "\n######################\n RELEASE UNSUCCESSFUL \n######################\n"
