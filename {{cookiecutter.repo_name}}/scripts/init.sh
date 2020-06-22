d19df325a7c92d1667b8c386181c62740717258a
André Cloete <andre.cloete@anmut.co.uk>
2020-06-22 16:52:14 +0100

    update init script

--- {{cookiecutter.repo_name}}/scripts/init.sh
+++ {{cookiecutter.repo_name}}/scripts/init.sh
@@ -8,6 +8,19 @@ echo "-> configure git hooks"
 pre-commit install -t pre-commit
 pre-commit install -t pre-push
 
+echo "-> creating initial commit in master" \
+&& git add . \
+&& git commit -m "initial commit" --no-verify \
+&& (
+	git remote add origin git@github.com:anmut-consulting/{{ cookiecutter.repo_name }}.git \
+	|| echo "WARNING: problem with remote.  Please verify.") \
+&& echo "Creating 'latest' tag" \
+&& git tag -f lastest \
+&& git push -u origin/master --no-verify --tags \
+&& echo "  -> adding dev branch" \
+&& git checkout -b dev \
+&& git push -u origin/dev
+
 echo
 echo "#################################################"
 echo "REPO FULLY INITIALISED! You're set up to develop."
