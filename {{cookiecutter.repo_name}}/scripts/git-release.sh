412764530cd639ef93b4f8567bb4fc0203262a7d
André Cloete <andre.cloete@anmut.co.uk>
2020-07-09 13:12:28 +0100

    fix: clean up git-release script

--- {{cookiecutter.repo_name}}/scripts/git-release.sh
+++ {{cookiecutter.repo_name}}/scripts/git-release.sh
@@ -1,20 +1,26 @@
-( echo "\n-> Tagging $1 release\n" \
-  && pipenv run bumpversion $1 --verbose \
-  && echo "Creating 'latest' tag" \
-  && git tag -f latest \
-  && echo "\n-> make documentation\n" \
-  && pipenv run make_docs \
-  && echo "\n-> docs made! use `open docs/_build/html/index.html` to view")
-( echo "\n-> pushing to remote, including tags\n" \
-  && ( pipenv run git push origin master --tags -f \
-    || pipenv run git push origin master --tags -f --no-verify
-    ) \
-  || ( git add Pipfile.lock \
-    && git commit -m "Update Pipfile.lock" --no-verify \
-    && ( pipenv run git push origin master --tags -f \
-      || pipenv run git push origin master --tags -f --no-verify
-      )
-    ) \
-&& echo "\n######################\nNEW RELEASE SUCCESSFUL\n######################\n"
-) \
-|| echo "\n######################\n RELEASE UNSUCCESSFUL \n######################\n"
+{
+    echo "\n-> Tagging $1 release\n" &&
+        pipenv run bumpversion $1 --verbose &&
+        echo "Creating 'latest' tag" &&
+        git tag -f latest &&
+        echo "\n-> make documentation\n" &&
+        pipenv run make_docs &&
+        echo "\n-> docs made! use $(open docs/_build/html/index.html) to view"
+} &&
+    {
+        echo "\n-> pushing to remote, including tags\n" &&
+            {
+                pipenv run git push origin master --tags -f ||
+                    pipenv run git push origin master --tags -f --no-verify
+            } ||
+            {
+                git add Pipfile.lock &&
+                    git commit -m "Update Pipfile.lock" --no-verify &&
+                    {
+                        pipenv run git push origin master --tags -f ||
+                            pipenv run git push origin master --tags -f --no-verify
+                    }
+            } &&
+            echo "\n######################\nNEW RELEASE SUCCESSFUL\n######################\n"
+    } ||
+    echo "\n######################\n RELEASE UNSUCCESSFUL \n######################\n"
